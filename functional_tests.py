f05fd1fd0265e9a1204fb4c15a0b320141c4d9ea
Bogdan Chira <bogdan.chira@yahoo.com>
2024-04-22 12:14:02 +0100

    Second commit - dependencies.txt and Tribe app added

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+# David hears about this new cool game. He goes to check out it's homepage
 browser.get('http://localhost:8000')
 
+# He notices that the game has a "Congratulation" word in the title
 assert 'Congratulations' in browser.title
+
+# He goes and he checks the first page of the game where he understood that the Tests tribe have their village
+# and notices that the page is called Tests Village
+
+# He clicks on the main house and reads about the
+
+browser.quit()
