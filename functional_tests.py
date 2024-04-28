d740a97cab6fb007f4e80b73c8763f075514e4d2
Bogdan Chira <bogdan.chira@yahoo.com>
2024-04-28 19:14:56 +0100

    Unit tests refactored

--- functional_tests.py
+++ functional_tests.py
@@ -20,24 +20,36 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('Tribe Home', self.browser.title, '"Tribe Home" not in page title, check page title')
 
         # He also notices that there is another page called story and he decides to check it out
+
         self.browser.get('http://localhost:8000/story')
         self.assertIn('Story', self.browser.title, '"Story" not in page title, check page title')
         # the page is empty for now, but he goes to the third page which is called "My Notes" and he notices
         # that the name of the page is "My Notes"
+
         self.browser.get('http://localhost:8000/my_notes')
         self.assertIn('My Notes', self.browser.title, '"My Notes" not in page title, check page title')
-        # He notices that on the "My notes page" there is a header called "My Journey Notes"
+        # He notices that on the "My notes" page"there is a header called "My Journey Notes"
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('My Journey Notes', header_text, 'There is no "My Journey Notes" h1 element')
-        # below, there is a logo of a goat
-        # and below that there is a text box that says 'your name'
+        # he is invited to write his first note in there
+        new_note = self.browser.find_element(By.ID, 'new_note_field')
+        self.assertEqual(new_note.get_attribute('placeholder'), 'Enter a new note')
+        # he types "Explore the story"
+        new_note.send_keys('Explore the story')
+        # he presses "Enter" and the page displays 1: Explore the story
+        new_note.send_keys(Keys.RETURN)
+        time.sleep(1)
+        table = self.browser.find_element(By.ID, 'table_of_notes')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Explore the story' for row in rows))
+
         # he enters his name and presses enter and the greeting message changes to his name
         # and the name field disappears
         # Upon checking the pages individually by url, he checks that all the navigation links bring up the page they
         # are designed to do
 
 
-        # self.fail('Test is not finished, finish the test')
+        self.fail('Test is not finished, finish the test')
 
 
 if __name__ == '__main__':
--- testtribe/Tribe/tests.py
+++ testtribe/Tribe/tests.py
@@ -5,61 +5,16 @@ from Tribe.views import homepage, story, my_notes
 
 class HomepageTest(TestCase):
 
-    # ============ tests that the path in urls.py calls the right function in views.py ============
-    def test_root_url_resolves_to_homepage_view(self):
-        # tests if homepage routs to the right views function
-        returned_object_containing_function = resolve('/')  # needs "/" before path even if in urls.py does not exist
-        self.assertEqual(returned_object_containing_function.func, homepage)
-
-    def test_credits_url_calls_proper_view_function(self):
-        # this one uses the resolve function Django uses when he searches for the view function associated with a path
-        # in this case ('/story') the path is the path to the story page
-        # in this case resolve eiter finds the path and returns an object containing the function associated
-        # or throws a juicy 404 error
-        returned_object_containing_function = resolve('/story')  # needs "/" before path even if in urls.py does not exist
-        # this one tests if the .function parameter of the returned object is the function that is called in views
-        self.assertEqual(returned_object_containing_function.func, story)
-
-    def test_my_notes_url_calls_proper_view_function(self):
-        returned_object_containing_function = resolve('/my_notes')
-        self.assertEqual(returned_object_containing_function.func, my_notes, '"My Notes" url did not returned the expected my_notes function')
-
-
-
-    # ============ tests that the views.py function returns the right .html file ============
-
     def test_homepage_function_returns_the_right_html(self):
-        # tests the homepage
-        request = HttpRequest
-        response = homepage(request)
-        html = response.content.decode('utf-8')
-        # tests that there is a html returned
-        self.assertTrue(html.startswith('\n<!DOCTYPE html>'), 'Seems like your returned file is not a html file')
-        # tests the title of the page
-        self.assertIn('<title>Test Tribe Home</title>', html, 'The name of the page is not the expected one')
-        # tests that the page loaded entirely
-        self.assertTrue(html.endswith('</html>'))
-
+        # makes a request, goes through the views function and returns a response containing the html
+        response = self.client.get('/')
+        # tests what template was used by the function
+        self.assertTemplateUsed(response, 'Tribe/homepage.html')
 
     def test_credit_returns_right_html(self):
-        # standard request object, method==get
-        request = HttpRequest()
-        # simulates a request to the story function I have defined in views.py that should return
-        # a response object with various data, including the story.html.
-        response = story(request)
-        # extract the html as a string
-        html = response.content.decode('utf-8')
-        # check that is indeed a html file you got back (I am inheriting from base.html, so it starts with a newline)
-        self.assertTrue(html.startswith('\n<!DOCTYPE html>'), 'Seems like your returned file is not a html file')
-        # checks that the title of the page is <title>Credits</title>
-        self.assertIn('<title>The Story</title>', html, 'The name of the page is not the expected one')
-        # checks that the file ends with <html> -that the whole page loaded was loaded
-        self.assertTrue(html.endswith('</html>'), 'Looks like the page didn`t loaded all the way down')
+        response = self.client.get('/story')
+        self.assertTemplateUsed(response, 'Tribe/story.html')
 
     def test_my_notes_function_returns_the_right_webpage(self):
-        request = HttpRequest()
-        response = my_notes(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('\n<!DOCTYPE html>'), 'Seems like your returned file is not a html file')
-
-        self.assertTrue(html.endswith('</html>'), 'Looks like the page didn`t loaded all the way down')
+        response = self.client.get('/my_notes')
+        self.assertTemplateUsed(response, 'Tribe/my_notes.html')
