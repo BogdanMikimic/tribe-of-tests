49d51b6d943df0c6efbbcc88e74dc48c045f8c38
Bogdan Chira <bogdan.chira@yahoo.com>
2024-04-29 13:49:25 +0100

    Database and initial database tests added

--- testtribe/Tribe/models.py
+++ testtribe/Tribe/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Note(models.Model):
+    """
+    Creates user notes for the user
+    """
+    text = models.CharField(max_length=300)
--- testtribe/Tribe/tests.py
+++ testtribe/Tribe/tests.py
@@ -1,12 +1,37 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
 from Tribe.views import homepage, story, my_notes
+from Tribe.models import Note
 
-class HomepageTest(TestCase):
+
+class DatabaseTests(TestCase):
+    """
+    Test the database tables and entries
+    """
+    def test_saving_and_receiving_notes(self):
+        # create 2 note objects and save them in the database
+        first_note_object = Note()
+        first_note_object.text = 'The first (ever) note!'
+        first_note_object.save()
+
+        second_note_object = Note()
+        second_note_object.text = 'Note the second'
+        second_note_object.save()
+
+        # retrieve the objects from the database
+        saved_items = Note.objects.all()
+
+        # check that they are 2
+        self.assertEqual(len(saved_items), 2)
+
+        # check that they contain the right data
+        self.assertEqual(saved_items[0].text, 'The first (ever) note!')
+        self.assertEqual(saved_items[1].text, 'Note the second')
+
+
+class PagesRenderingTests(TestCase):
 
     def test_homepage_function_returns_the_right_html(self):
-        # makes a request, goes through the views function and returns a response containing the html
+        # makes a request at a certain url, goes through the views function and returns a response containing the html
         response = self.client.get('/')
         # tests what template was used by the function
         self.assertTemplateUsed(response, 'Tribe/homepage.html')
@@ -18,3 +43,8 @@ class HomepageTest(TestCase):
     def test_my_notes_function_returns_the_right_webpage(self):
         response = self.client.get('/my_notes')
         self.assertTemplateUsed(response, 'Tribe/my_notes.html')
+
+    def test_post_request_and_response(self):
+        # make a post request to the form field name = item_text with the data 'A new note'
+        response = self.client.post('/my_notes', data={'item_text': 'A new note'})
+        self.assertIn('A new note', response.content.decode())
